ec287467a52309ca4485d6cf759e5cd3da0b9ce5
Roberto Cosenza <nospam@robcos.com>
2010-07-23 14:47:13 +0100

    added check for clean client

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,19 @@ echo "Deploying new version of branch"
 git branch | grep "*"
 echo "##############################"
 echo 
+
+
+clean=$(git status | grep -c "working directory clean")
+[ "$clean" == "1" ] || {
+  echo "Commit your changes first"
+  exit
+}
+
 git log -n 1 | head -1 >templates/version.txt
 
 echo last commit
 cat templates/version.txt
 echo
+
 
 appcfg.py update ../Viaggiare-Nel-Pollino/ -e robcos@robcos.com
